0052bbb8cc82d079036733a5b172c716caca267d
Urja <urjathakur5@gmail.com>
2020-05-10 09:54:40 +0530

    Added day 9 code

---
+++ ValidPerfectSquare.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        long x=num;
+        while(x*x>num)
+        {
+            x=(x+num/x)/2;
+        }
+        if(x*x==num)
+            return true;
+        return false;
+    }
+};
